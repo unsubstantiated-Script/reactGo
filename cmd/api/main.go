91efc9047ebd5e689e93e765141da62a5dc654fc
unsubstantiated-Script <jusfulton@gmail.com>
2025-05-31 07:18:12 -0700

    Setting up the server

---
+++ cmd/api/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = 8080
+
+type application struct {
+	Domain string
+}
+
+func main() {
+	// Set app config
+	var app application
+
+	// read from  CLI
+	// connect to DB
+
+	app.Domain = "example.com"
+
+	log.Printf("Starting server on port %d", port)
+
+	http.HandleFunc("/", Hello)
+
+	// Starting up; the server.
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Error starting server: %v\n", err))
+	}
+
+}
