c359ced6959ed08b822ee149c6cb62b3f58e1edf
unsubstantiated-Script <jusfulton@gmail.com>
2025-06-01 05:48:15 -0700

    Making the write JSON a utility

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -17,19 +16,7 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 		Version: "1.0.0",
 	}
 
-	// Set the content type header to application/json
-	out, err := json.Marshal(payload)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	_, err = w.Write(out)
-
-	if err != nil {
-		fmt.Println(err)
-	}
+	_ = app.writeJSON(w, http.StatusOK, payload)
 }
 
 func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
@@ -40,17 +27,5 @@ func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Set the content type header to application/json
-	out, err := json.Marshal(movies)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	_, err = w.Write(out)
-
-	if err != nil {
-		fmt.Println(err)
-	}
+	_ = app.writeJSON(w, http.StatusOK, movies)
 }
