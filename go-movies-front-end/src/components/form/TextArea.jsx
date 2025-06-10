22eb8d7b499f989dc922d11b6b1121e6101bcdbe
unsubstantiated-Script <jusfulton@gmail.com>
2025-06-09 20:05:00 -0700

    New input components

---
+++ go-movies-front-end/src/components/form/TextArea.jsx
@@ -0,0 +1,18 @@
+const TextArea = (props) => {
+    return (
+        <div className="mb-3">
+            <label htmlFor={props.name} className="form-label">{props.title}</label>
+            <textarea
+                className="form-control"
+                id={props.name}
+                name={props.name}
+                onChange={props.onChange}
+                value={props.value}
+                rows={props.rows || 3}
+            />
+            <div className={props.errorDiv}>
+                {props.errorMsg}
+            </div>
+        </div>
+    )
+}
